8c61b32e6c92715c6865974f901093eaac6f74d0
Viktor Braun (v_b) <mail@viktor-braun.de>
2019-11-13 22:32:19 +0100

    implemented button state

--- light/ContentView.swift
+++ light/ContentView.swift
@@ -2,15 +2,17 @@ import SwiftUI
 
 struct ContentView: View {
     @State var btnSize = CGFloat(130)
+    @State var lightOn = false
     var body: some View {
         ZStack(alignment: .center){
             Button(action: {
+                self.lightOn.toggle()
             }){
-                Text("Light")
+                Text(self.lightOn ? "DARKNESS" : "LIGHT")
                     .frame(width: self.btnSize, height: self.btnSize)
-                    .foregroundColor(Color.white)
+                    .foregroundColor(self.lightOn ? Color.white : Color.black)
             }
-            .background(Color.black)
+            .background(self.lightOn ? Color.black : Color.white)
             .cornerRadius(self.btnSize / 2)
             
             
